0df7e000fd8e4fc634b394113e2895320e29bdcd
Макс Крутоног <maxkrutonog@MacBook-Pro-Maks.local>
2020-09-02 22:18:58 +0300

    Done: able to fetch pages and parse their content

--- stock_collect/__init__.py
+++ stock_collect/__init__.py
@@ -1,17 +1,25 @@
 from requests import Session
-from re import compile
+from re import findall, sub
 from json import loads
+from bs4 import BeautifulSoup
 
 
-def get_news_pages(website):
-    assert list(website.keys()) == ["url", "page_pattern", "content_tags", "time_tags"]
-    with Session() as sess:
-        res = sess.get(website["url"])
-        if not res.ok:
-            return []
-        contents = res.content.decode("utf-8")
-        page_pattern = compile(website["page_pattern"])
-        return page_pattern.findall(contents)
+def fetch_pages_urls(url, page_pattern, session):
+    res = session.get(url)
+    if not res.ok:
+        return []
+    contents = res.content.decode("utf-8")
+    return findall(page_pattern, contents)
+
+
+def get_page_contents(content_tags, page_url, session):
+    res = session.get(page_url)
+    if not res.ok:
+        return ""
+    soup = BeautifulSoup(res.content.decode("utf-8"), features="html.parser")
+    content = ''.join(
+        (''.join((tag.text for tag in soup.select(content_tag)))) for content_tag in content_tags)
+    return content
 
 
 if __name__ == "__main__":
@@ -20,6 +28,16 @@ if __name__ == "__main__":
     if not websites:
         exit()
     news_pages = {}
-    for website in websites:
-        news_pages[website["url"]] = get_news_pages(website)
-    print(news_pages)
+    with Session() as sess:
+        for website in websites:
+            pages_urls = fetch_pages_urls(website["url"], website["page_pattern"], sess)[:10]
+            news_pages[website["url"]] = {
+                "urls": pages_urls,
+                "contents": [get_page_contents(
+                    website["content_tags"], url, sess) for url in pages_urls]
+            }
+    for key, value in news_pages.items():
+        print(key)
+        print(*value["contents"], sep="\n\n")
+
+
