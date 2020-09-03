fe09c1dc8fec1bee2f7d4539946d34cefd2365c8
Макс Крутоног <maxkrutonog@MacBook-Pro-Maks.local>
2020-09-03 16:42:00 +0300

    Done: created 'get_news' which will later be used in lifecycle

--- stock_collect/__init__.py
+++ stock_collect/__init__.py
@@ -1,7 +1,7 @@
 from requests import Session
 from re import findall, sub
 from json import loads, dumps
-from datetime import datetime
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from os.path import join
 
@@ -14,27 +14,43 @@ def fetch_page_urls(url, page_pattern, session):
     return findall(page_pattern, contents)
 
 
-def get_page_contents(website, page_url, session):
+def merge_tags(soup, tag_names):
+    return "".join("".join(t.text for t in soup.select(tag)) for tag in tag_names)
+
+
+def get_page_data(website, page_url, session):
     res = session.get(page_url)
     if not res.ok:
         return ""
     soup = BeautifulSoup(res.content.decode("utf-8"), features="html.parser")
-    content = ''.join(
-        ''.join(tag.text for tag in soup.select(content_tag)) for content_tag in website["content_tags"])
-    time = "".join(
-        "".join(t.text for t in soup.select(time_tag)) for time_tag in website["time_tags"])
-    time = datetime.strptime(time, website["time_format"])
-    return content, time
+    content, time = (merge_tags(soup, website[tags]) for tags in ("content_tags", "time_tags"))
+    return content, datetime.strptime(time, website["time_format"])
 
 
-def dump_news(news_content, dumping_folder):
+def filter_news(time_lower, time_upper, news):
+    return [_news[0] for _news in news if time_lower <= _news[1] <= time_upper]
+
+
+def dump_news(dumping_folder, news_content, time_stamp):
     """{timestamp: 18:00, content: "Everyone knows Donald Tru...",  TODO: stocks: [...],  TODO: stock_slope: 3.22,}"""
-    dt_now = datetime.now()
-    with open(join(dumping_folder, f"{dt_now.day}_{dt_now.month}_{dt_now.year}.dump"), "a") as f:
+    with open(join(dumping_folder, f"{time_stamp.day}_{time_stamp.month}_{time_stamp.year}.dump"), "a") as f:
         f.write("\n" + dumps({
-            "timestamp": f"{dt_now.hour}:{dt_now.minute}",
+            "timestamp": f"{time_stamp.hour}:{time_stamp.minute}",
             "content": news_content,
         }))
+
+
+def get_news(interval, dumping_folder="/dumps"):
+    with open("websites.json", "r") as f:
+        websites = loads(f.read())
+    if not websites:
+        return print("Unable to load websites")
+    pages, dt_now = [], datetime.now()
+    with Session() as sess:
+        for website in websites:
+            urls = fetch_page_urls(website["url"], website["page_pattern"], sess)
+            pages.extend(filter_news(dt_now, dt_now + interval, [get_page_data(website, url, sess) for url in urls]))
+    dump_news(dumping_folder, " ".join(pages), dt_now)
 
 
 if __name__ == "__main__":
@@ -46,11 +62,4 @@ if __name__ == "__main__":
     with Session() as sess:
         for website in websites:
             pages_urls = fetch_page_urls(website["url"], website["page_pattern"], sess)[:10]
-    #         news_pages[website["url"]] = {
-    #             "urls": pages_urls,
-    #             "contents": [get_page_contents(
-    #                 website["content_tags"], url, sess) for url in pages_urls]
-    #         }
-    # for key, value in news_pages.items():
-    #     dump_news(" ".join(value["contents"]), "dumps")
-        get_page_contents(website, pages_urls[0], sess)
+        get_page_data(website, pages_urls[0], sess)
