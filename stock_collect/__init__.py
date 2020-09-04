40117fc6e6773a21010055cfd2c7aa5c2f72ad47
Макс Крутоног <maxkrutonog@MacBook-Pro-Maks.local>
2020-09-04 14:30:30 +0300

    Done: added methods to lem. and process words; changed format of dumps - words are stored as list

--- stock_collect/__init__.py
+++ stock_collect/__init__.py
@@ -7,6 +7,9 @@ from os.path import join
 from threading import Timer
 from dateparser import parse
 from pytz import utc
+from nltk import wordnet, pos_tag, WordNetLemmatizer
+from nltk.corpus import stopwords
+import re
 
 
 def fetch_page_urls(url, page_pattern, session):
@@ -38,10 +41,39 @@ def filter_news(time_lower, time_upper, news):
 def dump_news(dumping_folder, news_content, time_stamp):
     """{timestamp: 18:00, content: "Everyone knows Donald Tru...",  TODO: stocks: [...],  TODO: stock_slope: 3.22,}"""
     with open(join(dumping_folder, f"{time_stamp.day}_{time_stamp.month}_{time_stamp.year}.dump"), "a+") as f:
-        f.write("\n" + dumps({
+        f.write(dumps({
             "timestamp": f"{time_stamp.hour}:{time_stamp.minute}",
             "content": news_content,
-        }))
+        }) + "\n")
+
+
+def get_postag(tag):
+    switch = {
+        "J": wordnet.wordnet.ADJ,
+        "V": wordnet.wordnet.VERB,
+        "N": wordnet.wordnet.NOUN,
+        "R": wordnet.wordnet.ADV,
+    }
+    for key, item in switch.items():
+        if tag.startswith(key):
+            return item
+    return switch["N"]
+
+
+def tag_words(words):
+    return [(word, get_postag(tag[1])) for word, tag in zip(words, pos_tag(words))]
+
+
+def lemmatize_words(words: list):
+    lem = WordNetLemmatizer()
+    return [lem.lemmatize(word, tag) for word, tag in tag_words(words)]
+
+
+def process_news(news):
+    pt = re.compile(r"\b([a-zA-Z]+[/\-&][a-zA-Z]+|[a-zA-Z]+|\d{4})\b")
+    sw = stopwords.words("english")
+    return lemmatize_words(
+        [t for t in pt.findall(news.replace('\xa0', ' ').lower()) if t and t not in sw and len(t) > 1])
 
 
 def fetch(interval, dumping_folder="dumps", start_time=datetime.now()):
@@ -54,8 +86,8 @@ def fetch(interval, dumping_folder="dumps", start_time=datetime.now()):
         for website in websites:
             urls = fetch_page_urls(website["url"], website["page_pattern"], sess)
             news = [get_page_data(website, url, sess) for url in urls]
-            pages.extend(filter_news(start_time, start_time + interval, news))
-    dump_news(dumping_folder, " ".join(pages), start_time)
+            pages.extend(filter_news(start_time - interval, start_time, news))
+    dump_news(dumping_folder, process_news(" ".join(pages)), start_time)
 
 
 def start_fetching(interval, dumping_folder="dumps", start_time=datetime.now()):
